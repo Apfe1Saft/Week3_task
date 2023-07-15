43ba74b799631e9baaf1d5f53945aa2dc66cfa7c
Alexander Ivanov <76427034+Apfe1Saft@users.noreply.github.com>
2023-07-15 06:58:51 +0000

    Update 10 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import "./styles.css";
+fetch("https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff")
+  .then((response) => response.json())
+  .then((data) => {
+    // Extract the necessary data from the JSON
+    const municipalities = data.dataset.dimension.Alue.category.label;
+    const populationData = data.dataset.value;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+    // Populate the table with the fetched data
+    const tableBody = document.getElementById("dataRows");
+    for (let i = 0; i < municipalities.length; i++) {
+      const row = document.createElement("tr");
+      const municipalityCell = document.createElement("td");
+      municipalityCell.textContent = municipalities[i];
+      const populationCell = document.createElement("td");
+      populationCell.textContent = populationData[i];
+      row.appendChild(municipalityCell);
+      row.appendChild(populationCell);
+      tableBody.appendChild(row);
+    }
+  })
+  .catch((error) => console.error("Error:", error));
